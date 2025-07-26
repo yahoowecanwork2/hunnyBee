2cd5e52fcc6dc14e8396671ae59ccd821be38d0c
Neha Yadav <ny663922@gmail.com>
2025-07-27 02:06:51 +0530

    making hero section responsive

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const bar = document.getElementById('bar');
+const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
-
-bar.addEventListener('click', () => {
-    nav.classList.toggle('active');
-});
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');//add
+    })
+};
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');//remove
+    })
+}
