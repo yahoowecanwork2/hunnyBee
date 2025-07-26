403a4882fb9ec301da0aa5722708ba10ed4e81dc
Neha Yadav <ny663922@gmail.com>
2025-07-26 18:40:47 +0530

    updated navbar

---
+++ script.js
@@ -0,0 +1,10 @@
+//navbar
+
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.toggle('active');
+    });
+}
